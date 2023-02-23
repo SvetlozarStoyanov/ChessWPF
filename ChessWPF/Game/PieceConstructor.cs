ca675fe7b71b8effa2ec2ac49691f5b30e78e5d3
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-02-23 21:16:30 +0200

    Added static method for constructing pieces in promotion menu. These pieces have no cell property

--- ChessWPF/Game/PieceConstructor.cs
+++ ChessWPF/Game/PieceConstructor.cs
@@ -6,24 +6,39 @@ namespace ChessWPF.Game
 {
     public static class PieceConstructor
     {
-        public static Piece ConstructPieceByType(Piece piece, Cell cell)
+        public static Piece ConstructPieceByType(PieceType pieceType,PieceColor color, Cell cell)
         {
-            switch (piece.PieceType)
+            switch (pieceType)
             {
                 case PieceType.Pawn:
-                    return new Pawn(piece.Color, cell);
+                    return new Pawn(color, cell);
                 case PieceType.Knight:
-                    return new Knight(piece.Color, cell);
+                    return new Knight(color, cell);
                 case PieceType.Bishop:
-                    return new Bishop(piece.Color, cell);
+                    return new Bishop(color, cell);
                 case PieceType.Rook:
-                    return new Rook(piece.Color, cell);
+                    return new Rook(color, cell);
                 case PieceType.Queen:
-                    return new Queen(piece.Color, cell);
+                    return new Queen(color, cell);
                 case PieceType.King:
-                    return new King(piece.Color, cell);
+                    return new King(color, cell);
             }
             return null;
+        }
+
+        public static Piece ConstructPieceForPromotion(PieceType pieceType, PieceColor Color)
+        {
+            switch (pieceType)
+            {
+                case PieceType.Knight:
+                    return new Knight(Color);
+                case PieceType.Bishop:
+                    return new Bishop(Color);
+                case PieceType.Rook:
+                    return new Rook(Color);
+                default:
+                    return new Queen(Color);
+            }
         }
     }
 }
