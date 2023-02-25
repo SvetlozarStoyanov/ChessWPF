8ba0ee275f2b85d37394eb7be6d7a43a05f4f2dc
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-02-25 17:21:00 +0200

    Added command to help with marking king when checked

---
+++ ChessWPF/Commands/CheckCommand.cs
@@ -0,0 +1,37 @@
+﻿using ChessWPF.Singleton;
+using ChessWPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessWPF.Commands
+{
+    public class CheckCommand : CommandBase
+    {
+        private readonly CellViewModel cellViewModel;
+        public CheckCommand(CellViewModel cellViewModel)
+        {
+            this.cellViewModel = cellViewModel;
+            cellViewModel.PropertyChanged += OnViewModelPropertyChanged;
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return cellViewModel.IsInCheck;
+        }
+        public override void Execute(object? parameter)
+        {
+            
+        }
+        private void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(CellViewModel.IsInCheck))
+            {
+                OnCanExecuteChanged();
+            }
+        }
+    }
+}
