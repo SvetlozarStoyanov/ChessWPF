860cd67e3ee6d5b7de864137d858e22590c313b6
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-02-25 17:18:20 +0200

    Separated promotion into its own command

--- ChessWPF/Commands/PromoteCommand.cs
+++ ChessWPF/Commands/PromoteCommand.cs
@@ -1,31 +1,36 @@
 ﻿using ChessWPF.Singleton;
 using ChessWPF.ViewModels;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ChessWPF.Commands
 {
-    public class SelectCommand : CommandBase
+    public class PromoteCommand : CommandBase
     {
         private readonly CellViewModel cellViewModel;
-        public SelectCommand(CellViewModel cellViewModel)
+        public PromoteCommand(CellViewModel cellViewModel)
         {
             this.cellViewModel = cellViewModel;
             cellViewModel.PropertyChanged += OnViewModelPropertyChanged;
         }
 
+
+
         public override bool CanExecute(object? parameter)
         {
-            return cellViewModel.CanBeSelected;
+            return cellViewModel.CanBeSelectedForPromotion;
         }
         public override void Execute(object? parameter)
         {
-            cellViewModel.IsSelected = true;
-            BackgroundSingleton.Instance.SelectCell(cellViewModel);
+            BackgroundSingleton.Instance.SelectPieceForPromotion(cellViewModel);
         }
         private void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
         {
-            if (e.PropertyName == nameof(CellViewModel.IsSelected) || e.PropertyName == nameof(CellViewModel.CanBeSelected))
+            if (e.PropertyName == nameof(CellViewModel.CanBeSelectedForPromotion))
             {
                 OnCanExecuteChanged();
             }
