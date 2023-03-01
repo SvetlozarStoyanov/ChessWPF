6e26784db5a81dac1493d431ac007b880490b7f9
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-03-01 20:42:00 +0200

    Added method which check if move is opposite of other move , equals method checks if move's cells are equal to other move's cells and removed unnecessary properties

--- ChessWPF/Models/Data/Board/Move.cs
+++ ChessWPF/Models/Data/Board/Move.cs
@@ -1,8 +1,9 @@
-﻿namespace ChessWPF.Models.Data.Board
+﻿using System.Reflection.Emit;
+
+namespace ChessWPF.Models.Data.Board
 {
     public class Move
     {
-        public int Number { get; set; }
         public Cell CellOneBefore { get; set; }
         public Cell CellOneAfter { get; set; }
         public Cell CellTwoBefore { get; set; }
@@ -13,6 +14,29 @@
         public Cell? CellFourBefore { get; set; }
         public Cell? CellFourAfter { get; set; }
 
-        public bool IsCastling { get; set; }
+        public override bool Equals(object? obj)
+        {
+            var otherMove = obj as Move;
+            if (this.CellOneBefore.Equals(otherMove.CellOneBefore) 
+                && this.CellTwoBefore.Equals(otherMove.CellTwoBefore)
+                && this.CellOneAfter.Equals(otherMove.CellOneAfter)
+                && this.CellTwoAfter.Equals(otherMove.CellTwoAfter))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsOppositeMove(Move otherMove)
+        {
+            if (this.CellOneBefore.Equals(otherMove.CellTwoAfter)
+            && this.CellTwoBefore.Equals(otherMove.CellOneAfter)
+            && this.CellOneAfter.Equals(otherMove.CellTwoBefore)
+            && this.CellTwoAfter.Equals(otherMove.CellOneBefore))
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
