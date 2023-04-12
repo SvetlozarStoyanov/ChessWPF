2c79a7330afe1c68d4fc6f281d32d420c4015cf4
Svetlozar Stoyanov <svetlio214@gmail.com>
2023-04-12 15:20:11 +0300

    Added GameStatus property which shows whose move it is or game result when game is finished.

--- ChessWPF/ViewModels/MenuViewModel.cs
+++ ChessWPF/ViewModels/MenuViewModel.cs
@@ -1,17 +1,34 @@
 ﻿using ChessWPF.Commands;
 using ChessWPF.Singleton;
+using System;
+using System.ComponentModel;
 using System.Windows.Input;
 
 namespace ChessWPF.ViewModels
 {
-    public class MenuViewModel : ViewModelBase
+    public class MenuViewModel : ViewModelBase, INotifyPropertyChanged
     {
-        public MenuViewModel()
+        private string gameStatus;
+        public MenuViewModel(BoardViewModel boardViewModel)
         {
-            ResetBoardCommand = new ResetBoardCommand(BackgroundSingleton.Instance.BoardViewModel);
-            UndoMoveCommand = new UndoMoveCommand(BackgroundSingleton.Instance.BoardViewModel);
+            ResetBoardCommand = new ResetBoardCommand(boardViewModel);
+            UndoMoveCommand = new UndoMoveCommand(boardViewModel);
+        }
+        public string GameStatus
+        {
+            get { return gameStatus; }
+            set
+            {
+                gameStatus = value;
+                OnPropertyChanged(nameof(GameStatus));
+            }
         }
         public ICommand ResetBoardCommand { get; set; }
         public ICommand UndoMoveCommand { get; set; }
+
+        public void UpdateGameStatus(string status)
+        {
+            GameStatus = status;
+        }
     }
 }
