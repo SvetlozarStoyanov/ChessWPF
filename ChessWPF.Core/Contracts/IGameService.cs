31fbf21a22204937894c65b4a18d2e774965831e
Svetlozar Stoyanov <94963053+SvetlozarStoyanov@users.noreply.github.com>
2023-07-03 10:50:22 +0300

    Delete ChessWPF.Core directory

--- ChessWPF.Core/Contracts/IGameService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ChessWPF.Infrastructure.Data.Board;
-
-namespace ChessWPF.Core.Contracts
-{
-    public interface IGameService
-    {
-        IEnumerable<Cell> GetLegalMoves<T>(Cell Cell, string color);
-    }
-}
