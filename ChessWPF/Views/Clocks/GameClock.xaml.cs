d86704b169e12ec99793c5fec19a44970bfbd543
SvetlozarStoyanov <svetlio214@gmail.com>
2023-09-26 15:17:08 +0300

    New view for game clocks

---
+++ ChessWPF/Views/Clocks/GameClock.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ChessWPF.Views.Clocks
+{
+    /// <summary>
+    /// Interaction logic for ChessCell.xaml
+    /// </summary>
+    public partial class GameClock : UserControl
+    {
+        public GameClock()
+        {
+            InitializeComponent();
+        }
+    }
+}
