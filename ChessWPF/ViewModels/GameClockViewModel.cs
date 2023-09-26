d19bfe94f03c80df314c22348a14ee37127821b8
SvetlozarStoyanov <svetlio214@gmail.com>
2023-09-26 15:19:52 +0300

    New view model for game clocks

---
+++ ChessWPF/ViewModels/GameClockViewModel.cs
@@ -0,0 +1,53 @@
+﻿using ChessWPF.Models.Data.Clocks;
+using ChessWPF.Models.Data.Pieces.Enums;
+using System;
+using System.ComponentModel;
+
+namespace ChessWPF.ViewModels
+{
+    public class GameClockViewModel : ViewModelBase, INotifyPropertyChanged
+    {
+        private GameClock gameClock;
+        private TimeSpan timeLeft;
+
+        public GameClockViewModel(PieceColor color)
+        {
+            gameClock = new GameClock(3, 10, color);
+        }
+
+        public GameClock GameClock
+        {
+            get { return gameClock; }
+        }
+
+        public TimeSpan TimeLeft
+        {
+            get => timeLeft;
+            set
+            {
+                timeLeft = value;
+                OnPropertyChanged(nameof(TimeLeft));
+            }
+        }
+
+
+        public void UpdateClock(TimeSpan timeLeft)
+        {
+            this.TimeLeft = timeLeft;
+        }
+
+        public void StartClock()
+        {
+            gameClock.StartClock();
+        }
+
+        public void StopClock()
+        {
+            gameClock.StopClock();
+        }
+        public void ResetClock()
+        {
+            gameClock.ResetClock();
+        }
+    }
+}
