68e1d4da49d9eb9a6a99f443398d7d64d7aae9dd
SvetlozarStoyanov <svetlio214@gmail.com>
2023-09-27 19:54:59 +0300

    Optimized tick, start, stop and reset methods . Added Add Increment method

--- ChessWPF/Models/Data/Clocks/GameClock.cs
+++ ChessWPF/Models/Data/Clocks/GameClock.cs
@@ -16,11 +16,6 @@ namespace ChessWPF.Models.Data.Clocks
         private TimeSpan timeLeft;
         private TimeSpan startingTime;
 
-        public TimeSpan StartingTime
-        {
-            get { return startingTime; }
-            private set { startingTime = value; }
-        }
 
 
         public GameClock(int increment, int startingTime, PieceColor color)
@@ -31,9 +26,9 @@ namespace ChessWPF.Models.Data.Clocks
             timeLeft = this.startingTime;
             timer = new DispatcherTimer();
 
-            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Interval = TimeSpan.FromMilliseconds(10);
             timer.Tick += new EventHandler(Timer_Tick);
-
+            watch = new Stopwatch();
         }
 
         public int Increment
@@ -53,31 +48,58 @@ namespace ChessWPF.Models.Data.Clocks
             set { timeLeft = value; }
         }
 
+        public TimeSpan StartingTime
+        {
+            get { return startingTime; }
+            private set { startingTime = value; }
+        }
+
         public void StartClock()
         {
             timer.Start();
+            watch.Start();
         }
 
         public void StopClock()
         {
+            watch.Stop();
             timer.Stop();
+            timeLeft -= watch.Elapsed;
+            watch.Reset();
+            BackgroundSingleton.Instance.GameViewModel.GameClocks[Color.ToString()].UpdateClock(timeLeft);
         }
 
         public void ResetClock()
         {
             timer.Stop();
+            watch.Stop();
+            watch.Reset();
             timeLeft = startingTime;
+        }
+
+        public void AddIncrement()
+        {
+            timeLeft += (TimeSpan.FromSeconds(increment));
+            BackgroundSingleton.Instance.GameViewModel.GameClocks[Color.ToString()].UpdateClock(timeLeft);
         }
 
         private void Timer_Tick(object? sender, EventArgs e)
         {
-            timeLeft -= TimeSpan.FromSeconds(1);
-            BackgroundSingleton.Instance.GameViewModel.GameClocks[Color.ToString()].UpdateClock(timeLeft);
-            if (timeLeft == TimeSpan.FromSeconds(0))
+            //timeLeft -= TimeSpan.FromMilliseconds(100);
+
+            if (timeLeft - watch.Elapsed <= TimeSpan.FromSeconds(0))
             {
+                watch.Stop();
                 timer.Stop();
+                watch.Reset();
                 var oppositeColor = this.color == PieceColor.White ? PieceColor.Black : PieceColor.White;
+                BackgroundSingleton.Instance.GameViewModel.GameClocks[Color.ToString()].UpdateClock(TimeSpan.FromSeconds(0));
                 BackgroundSingleton.Instance.EndGameByTimeOut(oppositeColor);
+            }
+            else
+            {
+                BackgroundSingleton.Instance.GameViewModel.GameClocks[Color.ToString()].UpdateClock(timeLeft - watch.Elapsed);
+                
             }
         }
 
