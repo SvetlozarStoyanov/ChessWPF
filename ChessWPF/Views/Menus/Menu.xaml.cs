3fd940666e7407c13a88eabce169b1ba33d56908
SvetlozarStoyanov <svetlio214@gmail.com>
2023-10-09 15:52:19 +0300

    Now takes whole length of the window. Changed background color

--- ChessWPF/Views/Menus/Menu.xaml.cs
+++ ChessWPF/Views/Menus/Menu.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace ChessWPF.Views.Menus
 {
@@ -10,6 +13,16 @@ namespace ChessWPF.Views.Menus
         public Menu()
         {
             InitializeComponent();
+            //gameStatusMenuItem.Style = this.FindResource("defaultMenuItem") as Style;
+            gameStatusMenuItem.FontWeight = FontWeights.Bold;
         }
+
+        private void Menu_Loaded(object sender, RoutedEventArgs e)
+        {
+            double controlsize = ((SystemParameters.PrimaryScreenWidth / 7) / 3 * 2) / 5 * 0.7;
+            Application.Current.Resources.Remove("MenuFontSize");
+            Application.Current.Resources.Add("MenuFontSize", controlsize);
+        }
+
     }
 }
