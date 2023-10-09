dcd53f8ac47e369e31217e5b59364a0bd38d457f
SvetlozarStoyanov <svetlio214@gmail.com>
2023-10-09 15:50:50 +0300

    Font sizes of time and color are now dynamically set

--- ChessWPF/Views/Clocks/GameClock.xaml.cs
+++ ChessWPF/Views/Clocks/GameClock.xaml.cs
@@ -15,5 +15,15 @@ namespace ChessWPF.Views.Clocks
         {
             InitializeComponent();
         }
+
+        private void GameClock_Loaded(object sender, RoutedEventArgs e)
+        {
+            double clockTimeSize = ((SystemParameters.PrimaryScreenWidth / 6) / 3 * 2) / 5 * 0.7;
+            Application.Current.Resources.Remove("ClockTimeFontSize");
+            Application.Current.Resources.Add("ClockTimeFontSize", clockTimeSize);
+            double clockColorSize = ((SystemParameters.PrimaryScreenWidth / 5) / 3 * 2) / 5 * 0.7;
+            Application.Current.Resources.Remove("ClockColorFontSize");
+            Application.Current.Resources.Add("ClockColorFontSize", clockColorSize);
+        }
     }
 }
