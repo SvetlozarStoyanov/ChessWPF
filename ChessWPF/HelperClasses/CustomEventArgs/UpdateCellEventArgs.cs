9a1649d7a440f54b1c77cd74fd7c43b192f36650
SvetlozarStoyanov <svetlio214@gmail.com>
2023-10-31 11:28:13 +0200

    New custom EventArgsClass Containing Piece property

---
+++ ChessWPF/HelperClasses/CustomEventArgs/UpdateCellEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using ChessWPF.Models.Data.Pieces;
+
+namespace ChessWPF.HelperClasses.CustomEventArgs
+{
+    public class UpdateCellEventArgs
+    {
+        public UpdateCellEventArgs(Piece? piece)
+        {
+            this.Piece = piece;
+        }
+        public Piece? Piece { get; }
+    }
+}
