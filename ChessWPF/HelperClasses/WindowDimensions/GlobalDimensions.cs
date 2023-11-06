63e232d3f2dff655b870def34eb5bed2ce7fef4b
SvetlozarStoyanov <svetlio214@gmail.com>
2023-11-06 15:00:54 +0200

    Added new static class which contains the dimensions (height and width ) of the  main window. They are changed when the window is resized.

---
+++ ChessWPF/HelperClasses/WindowDimensions/GlobalDimensions.cs
@@ -0,0 +1,8 @@
+﻿namespace ChessWPF.HelperClasses.WindowDimensions
+{
+    public static class GlobalDimensions
+    {
+        public static double Height { get; set; }
+        public static double Width { get; set; }
+    }
+}
