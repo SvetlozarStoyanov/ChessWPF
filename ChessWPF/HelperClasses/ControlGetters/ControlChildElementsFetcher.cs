cd97797063219aea1eb5a1899e3079cd4fe2173b
SvetlozarStoyanov <svetlio214@gmail.com>
2023-11-06 16:42:48 +0200

    Added class which fetches children of given type of element

---
+++ ChessWPF/HelperClasses/ControlGetters/ControlChildElementsFetcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ChessWPF.HelperClasses.ControlGetters
+{
+    public static class ControlChildElementsFetcher
+    {
+        public static List<T> GetChildrenOfType<T>(this DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj == null)
+            {
+                return null!;
+            }
+            var foundChildren = new List<T>();
+
+            var childrenCount = VisualTreeHelper.GetChildrenCount(depObj);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(depObj, i);
+                if (child.GetType() == typeof(T))
+                {
+                    foundChildren.Add((T)child);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return foundChildren;
+        }
+    }
+}
