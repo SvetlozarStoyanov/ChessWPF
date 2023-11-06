7d870da41883cdf4cf2c2a2a7d970ce89782e499
SvetlozarStoyanov <svetlio214@gmail.com>
2023-11-06 15:06:05 +0200

    Added size changed event which resizes menu elements font sizes when main window is resized.

--- ChessWPF/Views/Menus/Menu.xaml.cs
+++ ChessWPF/Views/Menus/Menu.xaml.cs
@@ -1,7 +1,6 @@
-﻿using System.Drawing;
+﻿using ChessWPF.HelperClasses.WindowDimensions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace ChessWPF.Views.Menus
 {
@@ -13,16 +12,23 @@ namespace ChessWPF.Views.Menus
         public Menu()
         {
             InitializeComponent();
-            //gameStatusMenuItem.Style = this.FindResource("defaultMenuItem") as Style;
             gameStatusMenuItem.FontWeight = FontWeights.Bold;
         }
 
         private void Menu_Loaded(object sender, RoutedEventArgs e)
         {
-            double controlsize = ((SystemParameters.PrimaryScreenWidth / 7) / 3 * 2) / 5 * 0.7;
-            Application.Current.Resources.Remove("MenuFontSize");
-            Application.Current.Resources.Add("MenuFontSize", controlsize);
+            var menuFontSize = GlobalDimensions.Width / 64;
+            newGameMenuItem.FontSize = menuFontSize;
+            undoMoveMenuItem.FontSize = menuFontSize;
+            gameStatusMenuItem.FontSize = menuFontSize;
         }
 
+        private void Menu_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var menuFontSize = GlobalDimensions.Width / 64;
+            newGameMenuItem.FontSize = menuFontSize;
+            undoMoveMenuItem.FontSize = menuFontSize;
+            gameStatusMenuItem.FontSize = menuFontSize;
+        }
     }
 }
