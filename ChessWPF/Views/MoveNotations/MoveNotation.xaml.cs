b828c73aa90d045ed4ed9ee480d123f53e77dd92
SvetlozarStoyanov <svetlio214@gmail.com>
2023-12-08 18:17:06 +0200

    New move containing both clocks and move notation

---
+++ ChessWPF/Views/MoveNotations/MoveNotation.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ChessWPF.Views.MoveNotations
+{
+    /// <summary>
+    /// Interaction logic for MoveNotation.xaml
+    /// </summary>
+    public partial class MoveNotation : UserControl
+    {
+        public MoveNotation()
+        {
+            InitializeComponent();
+        }
+
+        private void copyMoveNotationButton_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(moveNotationTextBox.Text);
+        }
+    }
+}
