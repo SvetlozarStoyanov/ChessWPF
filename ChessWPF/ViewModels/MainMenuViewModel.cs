de4e8b4f7438c2c908125f347952100f138e7829
SvetlozarStoyanov <svetlio214@gmail.com>
2024-01-24 17:21:26 +0200

    Added Main menu views and viewmodels

---
+++ ChessWPF/ViewModels/MainMenuViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ChessWPF.Commands;
+using ChessWPF.Stores;
+using System.Windows.Input;
+
+namespace ChessWPF.ViewModels
+{
+    public class MainMenuViewModel : ViewModelBase
+    {
+        public MainMenuViewModel(NavigationStore navigationStore)
+        {
+            NavigateToGameCommand = new NavigateCommand<GameViewModel>(navigationStore, () => new GameViewModel(navigationStore));
+        }
+        public ICommand NavigateToGameCommand { get; init; }
+    }
+}
