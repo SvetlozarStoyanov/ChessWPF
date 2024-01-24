184ed17ce8a34cebf56f7084fb3556ff386ac140
SvetlozarStoyanov <svetlio214@gmail.com>
2024-01-24 17:16:40 +0200

    Added navigation store class which serves as a mediator between the mainwindow and mainviewmodel and is responsible for navigating between views

---
+++ ChessWPF/Stores/NavigationStore.cs
@@ -0,0 +1,29 @@
+﻿using ChessWPF.ViewModels;
+using System;
+
+namespace ChessWPF.Stores
+{
+    public class NavigationStore
+    {
+        private ViewModelBase currentViewModel;
+        public NavigationStore()
+        {
+
+        }
+        public event Action CurrentViewModelChanged;
+        public ViewModelBase CurrentViewModel 
+        {
+            get => currentViewModel;
+            set
+            {
+                currentViewModel = value;
+                OnCurrentViewModelChanged();
+            }
+        }
+
+        private void OnCurrentViewModelChanged()
+        {
+            CurrentViewModelChanged?.Invoke();
+        }
+    }
+}
