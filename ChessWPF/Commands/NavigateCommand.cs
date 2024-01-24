4516199bd70c997f3f8266c051b9fbb87db52a43
SvetlozarStoyanov <svetlio214@gmail.com>
2024-01-24 17:24:16 +0200

    Added command which changes navigation store's currentViewModel. The command has a generic constructor and only accepts types which inherit from CommandBase abstract class

---
+++ ChessWPF/Commands/NavigateCommand.cs
@@ -0,0 +1,24 @@
+﻿using ChessWPF.Stores;
+using ChessWPF.ViewModels;
+using System;
+
+namespace ChessWPF.Commands
+{
+    public class NavigateCommand<TViewModel> : CommandBase 
+        where TViewModel : ViewModelBase
+    {
+        private NavigationStore navigationStore;
+        private readonly Func<TViewModel> createViewModel;
+
+        public NavigateCommand(NavigationStore navigationStore, Func<TViewModel> createViewModel)
+        {
+            this.navigationStore = navigationStore;
+            this.createViewModel = createViewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            navigationStore.CurrentViewModel = createViewModel();
+        }
+    }
+}
