c0c4c7c447eabbfd9cbbd0239342b67a442e0f76
SvetlozarStoyanov <svetlio214@gmail.com>
2024-01-24 17:19:21 +0200

    Cleaned up constructor, added navigation store property and subscribed to onproperty changed of navigation store's currentviewmodel.

--- ChessWPF/ViewModels/MainViewModel.cs
+++ ChessWPF/ViewModels/MainViewModel.cs
@@ -1,32 +1,22 @@
-﻿using ChessWPF.Models.Data.Pieces.Enums;
-using System.Collections.Generic;
-using System.Windows.Media;
+﻿using ChessWPF.Stores;
+using System;
 
 namespace ChessWPF.ViewModels
 {
     public sealed class MainViewModel : ViewModelBase
     {
-        public GameViewModel GameViewModel { get; set; }
-        public MainViewModel()
+        private NavigationStore navigationStore;
+        public MainViewModel(NavigationStore navigationStore)
         {
-            var boardViewModel = new BoardViewModel();
-            var menuViewModel = new MenuViewModel(boardViewModel);
-            var whiteGameClockViewModel = new GameClockViewModel(PieceColor.White,
-                Color.FromRgb(0, 100, 0),
-                Color.FromRgb(255, 255, 255),
-                Color.FromRgb(255, 0, 0));
+            this.navigationStore = navigationStore;
+            navigationStore.CurrentViewModelChanged += OnCurrentViewModelChanged;
+        }
 
-            var blackGameClockViewModel = new GameClockViewModel(PieceColor.Black,
-                Color.FromRgb(0, 100, 0),
-                Color.FromRgb(255, 255, 255),
-                Color.FromRgb(255, 0, 0));
-            var gameClocks = new Dictionary<string, GameClockViewModel>()
-            {
-                ["White"] = whiteGameClockViewModel,
-                ["Black"] = blackGameClockViewModel
-            };
-            GameViewModel = new GameViewModel(boardViewModel, menuViewModel, gameClocks);
-            GameViewModel.StartGame();
+        public ViewModelBase CurrentViewModel => navigationStore.CurrentViewModel;
+
+        private void OnCurrentViewModelChanged()
+        {
+            OnPropertyChanged(nameof(CurrentViewModel));
         }
     }
 }
