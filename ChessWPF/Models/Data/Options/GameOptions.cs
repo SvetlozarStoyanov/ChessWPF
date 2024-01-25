3eefc07dbb95c9955826746d3b07ab723291a811
SvetlozarStoyanov <svetlio214@gmail.com>
2024-01-25 16:56:09 +0200

    Added GameOptions class which contains information about the game (TimeControl)

---
+++ ChessWPF/Models/Data/Options/GameOptions.cs
@@ -0,0 +1,18 @@
+﻿using ChessWPF.Models.Data.Enums;
+
+namespace ChessWPF.Models.Data.Options
+{
+    public class GameOptions
+    {
+        public GameOptions(TimeControl timeControl)
+        {
+            TimeControl = timeControl;
+        }
+        public GameOptions() : this(new TimeControl(300, 5, TimeControlType.Blitz))
+        {
+
+        }
+
+        public TimeControl TimeControl { get; set; }
+    }
+}
