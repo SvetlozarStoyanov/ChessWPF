2d17f61c234d4135e607a5c1203d0c7ef144e6d6
SvetlozarStoyanov <svetlio214@gmail.com>
2024-01-25 17:00:08 +0200

    Added time control view model class

---
+++ ChessWPF/ViewModels/TimeControlViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ChessWPF.Commands;
+using ChessWPF.Models.Data.Options;
+using System;
+using System.Windows.Input;
+
+namespace ChessWPF.ViewModels
+{
+    public class TimeControlViewModel : ViewModelBase
+    {
+        public TimeControlViewModel(TimeControl timeControl)
+        {
+            TimeControl = timeControl;
+            SelectTimeControlCommand = new SelectTimeControlCommand(this);
+        }
+
+        public event Action Select;
+        public bool IsSelected { get; set; }
+
+        public string TimeAsText { get => TimeControl.ToString()!; } 
+        public TimeControl TimeControl { get; init; }
+        public ICommand SelectTimeControlCommand { get; }
+
+        public void SelectChanged()
+        {
+            this.IsSelected = true;
+            Select?.Invoke();
+        }
+    }
+}
