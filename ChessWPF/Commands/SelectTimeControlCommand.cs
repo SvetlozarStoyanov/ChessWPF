5231902580b9ee9b9240ca433090731b32d26014
SvetlozarStoyanov <svetlio214@gmail.com>
2024-01-25 17:00:40 +0200

    New command which calls TimeControlViewModel's SelectChanged method when executed

---
+++ ChessWPF/Commands/SelectTimeControlCommand.cs
@@ -0,0 +1,18 @@
+﻿using ChessWPF.ViewModels;
+
+namespace ChessWPF.Commands
+{
+    public class SelectTimeControlCommand : CommandBase
+    {
+        private readonly TimeControlViewModel timeControlViewModel;
+        public SelectTimeControlCommand(TimeControlViewModel timeControlViewModel)
+        {
+            this.timeControlViewModel = timeControlViewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            timeControlViewModel.SelectChanged();
+        }
+    }
+}
