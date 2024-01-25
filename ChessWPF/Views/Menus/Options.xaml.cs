72e9321098e28959858119186d31a771b44b9846
SvetlozarStoyanov <svetlio214@gmail.com>
2024-01-25 16:54:56 +0200

    Added options view

---
+++ ChessWPF/Views/Menus/Options.xaml.cs
@@ -0,0 +1,17 @@
+﻿using ChessWPF.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ChessWPF.Views.Menus
+{
+    /// <summary>
+    /// Interaction logic for Options.xaml
+    /// </summary>
+    public partial class Options : UserControl
+    {
+        public Options()
+        {
+            InitializeComponent();
+        }
+    }
+}
