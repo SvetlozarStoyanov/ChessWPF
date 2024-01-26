988cf6ba5b45aca036ca7bc0e190f765aede09dc
SvetlozarStoyanov <svetlio214@gmail.com>
2024-01-26 14:20:01 +0200

    Moved ItemsControl To separate userControl and split TimeControlViewModels into different collections depending on their type

---
+++ ChessWPF/Views/TimeControls/TimeControlList.xaml.cs
@@ -0,0 +1,37 @@
+﻿using ChessWPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChessWPF.Views.TimeControls
+{
+    /// <summary>
+    /// Interaction logic for TimeControlList.xaml
+    /// </summary>
+    public partial class TimeControlList : UserControl
+    {
+        public TimeControlList()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void ItemsControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            var list = (this.DataContext as List<TimeControlViewModel>);
+            itemsControl.ItemsSource = list;
+            timeControlTypeLabel.Content = list.First().TimeControl.TimeControlType;
+        }
+    }
+}
