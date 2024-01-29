67053cec51271139a0435359d80ff4fbabc597d4
SvetlozarStoyanov <svetlio214@gmail.com>
2024-01-29 13:42:01 +0200

    Added time control view

---
+++ ChessWPF/Views/TimeControls/TimeControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using ChessWPF.HelperClasses.WindowDimensions;
+using System.Windows.Controls;
+
+namespace ChessWPF.Views.TimeControls
+{
+    /// <summary>
+    /// Interaction logic for TimeControl.xaml
+    /// </summary>
+    public partial class TimeControl : UserControl
+    {
+        public TimeControl()
+        {
+            InitializeComponent();
+        }
+
+        //private void UserControl_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        //{
+        //    timeControlRadioButton.FontSize = GlobalDimensions.Width / 80;
+        //}
+
+        private void timeControlRadioButton_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        {
+            timeControlRadioButton.FontSize = GlobalDimensions.Width / 80;
+        }
+    }
+}
