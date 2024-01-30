5dd6998e034acccc5db8f0c42a50529932d3ffc1
SvetlozarStoyanov <svetlio214@gmail.com>
2024-01-30 21:01:19 +0200

    Added position creator static class

---
+++ ChessWPF/Game/PositionCreator.cs
@@ -0,0 +1,19 @@
+﻿using ChessWPF.Models.Data.Pieces;
+using ChessWPF.Models.Data.Pieces.Enums;
+using ChessWPF.Models.Positions;
+using System.Collections.Generic;
+
+namespace ChessWPF.Game
+{
+    public static class PositionCreator
+    {
+        public static Position CreateDefaultPosition()
+        {
+            var fenAnnotation = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+            var position = FenAnnotationReader.GetPosition(fenAnnotation);
+            position.FenAnnotation = fenAnnotation;
+
+            return position;
+        }
+    }
+}
