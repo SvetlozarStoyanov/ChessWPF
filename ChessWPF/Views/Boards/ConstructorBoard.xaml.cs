9388fcf3814f4ae12243dbd84feadf94403a87aa
SvetlozarStoyanov <svetlio214@gmail.com>
2024-01-30 20:59:37 +0200

    Added new views for board constructor feature

---
+++ ChessWPF/Views/Boards/ConstructorBoard.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ChessWPF.HelperClasses.ControlGetters;
+using ChessWPF.ViewModels;
+using ChessWPF.Views.Cells;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ChessWPF.Views.Boards
+{
+    /// <summary>
+    /// Interaction logic for ConstructorBoard.xaml
+    /// </summary>
+    public partial class ConstructorBoard : UserControl
+    {
+        private List<TextBlock> cellAnnotationTextBlocks;
+
+        public ConstructorBoard()
+        {
+            InitializeComponent();
+            cellAnnotationTextBlocks = ControlChildElementsFetcher.GetChildrenOfType<TextBlock>(mainGrid);
+        }
+
+        private void cellGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitializeChessCells();
+        }
+
+        private void InitializeChessCells()
+        {
+            var constructorCellViewModels = (this.DataContext as ConstructorCellViewModel[][])!;
+            for (int row = 0; row < constructorCellViewModels.Length; row++)
+            {
+                for (int col = 0; col < constructorCellViewModels[row].Length; col++)
+                {
+                    var chessCell = new ChessConstructorCell();
+                    chessCell.SetValue(Grid.RowProperty, row);
+                    chessCell.SetValue(Grid.ColumnProperty, col);
+                    chessCell.DataContext = constructorCellViewModels[row][col];
+                    cellGrid.Children.Add(chessCell);
+                }
+            }
+        }
+    }
+}
---
+++ ChessWPF/Views/Cells/ChessConstructorCell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ChessWPF.Views.Cells
+{
+    /// <summary>
+    /// Interaction logic for ChessConstructorCell.xaml
+    /// </summary>
+    public partial class ChessConstructorCell : UserControl
+    {
+        public ChessConstructorCell()
+        {
+            InitializeComponent();
+            updatePieceBtn.Style = this.FindResource("defaultBtn") as Style;
+        }
+    }
+}
