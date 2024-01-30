f115e67318b7ef88aa638d67e108022b5d3e0a85
SvetlozarStoyanov <svetlio214@gmail.com>
2024-01-30 21:01:51 +0200

    Added Position class.

---
+++ ChessWPF/Models/Positions/Position.cs
@@ -0,0 +1,41 @@
+﻿using ChessWPF.Models.Data.Pieces;
+using ChessWPF.Models.Data.Pieces.Enums;
+using System.Collections.Generic;
+
+namespace ChessWPF.Models.Positions
+{
+    public sealed class Position
+    {
+        private string fenAnnotation;
+        private PieceColor turnColor;
+        private Dictionary<PieceColor, List<Piece>> pieces;
+
+        public Position()
+        {
+            
+        }
+
+        public string FenAnnotation
+        {
+            get { return fenAnnotation; }
+            set { fenAnnotation = value; }
+        }
+
+        public PieceColor TurnColor
+        {
+            get { return turnColor; }
+            set { turnColor = value; }
+        }
+
+        public Dictionary<PieceColor, List<Piece>> Pieces
+        {
+            get { return pieces; }
+            set { pieces = value; }
+        }
+
+        private void UpdateFenAnnotation()
+        {
+
+        }
+    }
+}
