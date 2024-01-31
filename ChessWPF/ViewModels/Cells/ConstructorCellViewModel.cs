38c1057044ccca01842baf5690dadb5e65856a64
SvetlozarStoyanov <svetlio214@gmail.com>
2024-01-31 20:30:46 +0200

    Added commands for updating and selecting piece from cell. Added CanBeSelected property to enable/disable SelectPiece button from view

--- ChessWPF/ViewModels/Cells/ConstructorCellViewModel.cs
+++ ChessWPF/ViewModels/Cells/ConstructorCellViewModel.cs
@@ -1,5 +1,9 @@
-﻿using ChessWPF.Models.Data.Board;
+﻿using ChessWPF.Commands;
+using ChessWPF.HelperClasses.CustomEventArgs;
+using ChessWPF.Models.Data.Board;
+using ChessWPF.Models.Data.Pieces;
 using System;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
@@ -7,6 +11,7 @@ namespace ChessWPF.ViewModels
 {
     public class ConstructorCellViewModel : ViewModelBase
     {
+        private bool canBeSelected;
         private BitmapImage? cellImage;
         private SolidColorBrush backgroundBrush;
         private ConstructorCell constructorCell;
@@ -16,18 +21,35 @@ namespace ChessWPF.ViewModels
             ConstructorCell = constructorCell;
             ConstructorCell.Update += UpdateCellImage;
             BackgroundBrush = new SolidColorBrush(color);
+            UpdateConstructorCellCommand = new UpdateConstructorCellCommand(this);
+            SelectPieceFromCellCommand = new SelectPieceFromCellCommand(this);
+        }
+
+        public event UpdateConstructorCellViewModelEventHandler UpdateConstructorCellViewModel;
+        public delegate void UpdateConstructorCellViewModelEventHandler(object? sender, UpdateConstructorCellViewModelEventArgs e);
+        public event SelectPieceFromConstructorCellViewModelCellEventHandler SelectPieceFromConstructorCellViewModelCell;
+        public delegate void SelectPieceFromConstructorCellViewModelCellEventHandler(object? sender, SelectPieceFromConstructorCellViewModelEventArgs e);
+
+
+        public bool CanBeSelected
+        {
+            get { return canBeSelected; }
+            private set
+            {
+                canBeSelected = value;
+                OnPropertyChanged(nameof(CanBeSelected));
+            }
         }
 
         public BitmapImage? CellImage
         {
             get { return cellImage; }
             private set
-            { 
+            {
                 cellImage = value;
                 OnPropertyChanged(nameof(CellImage));
             }
         }
-
 
         public SolidColorBrush BackgroundBrush
         {
@@ -45,17 +67,45 @@ namespace ChessWPF.ViewModels
             set { constructorCell = value; }
         }
 
+        public ICommand UpdateConstructorCellCommand { get; init; }
+
+        public ICommand SelectPieceFromCellCommand { get; init; }
+
         public void UpdateCellImage(object? sender, EventArgs e)
         {
-            if (constructorCell.ConstructorPiece != null )
+            if (constructorCell.ConstructorPiece != null)
             {
-                var imageUrl = $"/Graphics/Chess Pieces/{constructorCell.ConstructorPiece.PieceColor} {constructorCell.ConstructorPiece.PieceType}.png";
+                var imageUrl = $"/Graphics/Chess Pieces/{ConstructorCell.ConstructorPiece.Color} {ConstructorCell.ConstructorPiece.PieceType}.png";
                 var pieceImageUri = new Uri(@$"pack://application:,,,{imageUrl}");
                 CellImage = new BitmapImage(pieceImageUri);
             }
             else
             {
                 CellImage = null;
+            }
+        }
+
+        public void UpdateCellPiece()
+        {
+            UpdateConstructorCellViewModel(null, new UpdateConstructorCellViewModelEventArgs(ConstructorCell.Row, ConstructorCell.Col));
+        }
+
+        public void SelectPiece()
+        {
+            SelectPieceFromConstructorCellViewModelCell(null,new SelectPieceFromConstructorCellViewModelEventArgs(ConstructorCell.Row,
+                ConstructorCell.Col,
+                ConstructorCell.ConstructorPiece!));
+        }
+
+        public void UpdateCanBeSelected(Type type)
+        {
+            if (type.Equals(typeof(BoardConstructorViewModel)))
+            {
+                CanBeSelected = !CanBeSelected;
+            }
+            else
+            {
+                throw new InvalidOperationException("Can only call from BoardConstructorViewModel");
             }
         }
     }
