d997813151a9c33f085ded1bea59bcc500c4896d
SvetlozarStoyanov <svetlio214@gmail.com>
2024-02-01 13:36:44 +0200

    Added Constructor Piece interface

---
+++ ChessWPF/Contracts/Pieces/IConstructorPiece.cs
@@ -0,0 +1,10 @@
+﻿using ChessWPF.Models.Data.Pieces.Enums;
+
+namespace ChessWPF.Contracts.Pieces
+{
+    public interface IConstructorPiece
+    {
+        public PieceColor Color { get;  }
+        public PieceType PieceType { get;  }
+    }
+}
