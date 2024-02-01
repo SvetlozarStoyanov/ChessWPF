ebb8f15afa8197895d537a59ff9d11fd9ff2cb7c
SvetlozarStoyanov <svetlio214@gmail.com>
2024-02-01 13:40:16 +0200

    Replaced SelectConstructorPieceEventArgs with  SelectMenuPieceEventArgs

---
+++ ChessWPF/HelperClasses/CustomEventArgs/SelectMenuPieceEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using ChessWPF.Models.Pieces;
+
+namespace ChessWPF.HelperClasses.CustomEventArgs
+{
+    public class SelectMenuPieceEventArgs
+    {
+        public SelectMenuPieceEventArgs(ConstructorMenuPiece? menuPiece)
+        {
+            MenuPiece = menuPiece;
+        }
+
+        public ConstructorMenuPiece? MenuPiece { get; init; }
+
+    }
+}
