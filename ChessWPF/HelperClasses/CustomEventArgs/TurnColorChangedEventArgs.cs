aff4f1c5aa44182a3e53b1d6c42db0cf2c3e1a6e
SvetlozarStoyanov <svetlio214@gmail.com>
2024-02-06 15:53:15 +0200

    New event args class for turn color changes

---
+++ ChessWPF/HelperClasses/CustomEventArgs/TurnColorChangedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using ChessWPF.Models.Data.Pieces.Enums;
+using System;
+
+namespace ChessWPF.HelperClasses.CustomEventArgs
+{
+    public class TurnColorChangedEventArgs : EventArgs
+    {
+        public TurnColorChangedEventArgs(PieceColor turnColor)
+        {
+            TurnColor = turnColor;
+        }
+
+        public PieceColor TurnColor { get; init; }
+    }
+}
