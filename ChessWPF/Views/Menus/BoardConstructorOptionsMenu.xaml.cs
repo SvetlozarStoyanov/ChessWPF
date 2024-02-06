85291e54c3078f4d0eb9bdd3f322b398a0ac605b
SvetlozarStoyanov <svetlio214@gmail.com>
2024-02-06 15:51:58 +0200

    Added BoardContructorOptionsMenu view to ChessBoardConstructor view. BoardConstructorOptionsMenu contains options for turn color,castling rights and en passant square(not implemented).

--- ChessWPF/Views/Menus/BoardConstructorOptionsMenu.xaml.cs
+++ ChessWPF/Views/Menus/BoardConstructorOptionsMenu.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ChessWPF.ViewModels;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ChessWPF.Views.Menus
 {
@@ -23,6 +12,19 @@ namespace ChessWPF.Views.Menus
         public BoardConstructorOptionsMenu()
         {
             InitializeComponent();
+        }
+
+        private void CheckBox_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            var checkbox = sender as CheckBox;
+            checkbox!.IsChecked = false;
+            checkbox.Command?.Execute(null);
+        }
+
+        private void turnColorsComboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            turnColorsComboBox.Items.Clear();
+            turnColorsComboBox.ItemsSource = (DataContext as BoardConstructorMenuViewModel)!.TurnColors;
         }
     }
 }
