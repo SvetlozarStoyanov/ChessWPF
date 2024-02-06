26a4012f95f4fee45f245a46ec2c7db1df8845a4
SvetlozarStoyanov <svetlio214@gmail.com>
2024-02-06 15:52:15 +0200

    New command for updating castling rights

---
+++ ChessWPF/Commands/SetCastlingRightsCommand.cs
@@ -0,0 +1,20 @@
+﻿using ChessWPF.ViewModels;
+using System;
+
+namespace ChessWPF.Commands
+{
+    public class SetCastlingRightsCommand : CommandBase
+    {
+        private readonly BoardConstructorMenuViewModel boardConstructorMenuViewModel;
+
+        public SetCastlingRightsCommand(BoardConstructorMenuViewModel boardConstructorMenuViewModel)
+        {
+            this.boardConstructorMenuViewModel = boardConstructorMenuViewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            boardConstructorMenuViewModel.UpdateCastlingRights();
+        }
+    }
+}
