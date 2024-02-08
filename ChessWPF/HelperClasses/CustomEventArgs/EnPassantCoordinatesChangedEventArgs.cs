239fed7b0e46a26fbc737dcbab3be21ef7f6e817
SvetlozarStoyanov <svetlio214@gmail.com>
2024-02-08 11:42:59 +0200

    Added event args for enPassantCoordinates changed

---
+++ ChessWPF/HelperClasses/CustomEventArgs/EnPassantCoordinatesChangedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using ChessWPF.Models.Cells;
+using System;
+
+namespace ChessWPF.HelperClasses.CustomEventArgs
+{
+    public class EnPassantCoordinatesChangedEventArgs : EventArgs
+    {
+        public EnPassantCoordinatesChangedEventArgs(CellCoordinates? cellCoordinates)
+        {
+            CellCoordinates = cellCoordinates;
+        }
+
+        public EnPassantCoordinatesChangedEventArgs()
+        {
+
+        }
+
+        public CellCoordinates? CellCoordinates { get; init; }
+    }
+}
