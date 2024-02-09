83e79da01d19b89ff2720a330915047f1d9b81f5
SvetlozarStoyanov <svetlio214@gmail.com>
2024-02-09 16:12:37 +0200

    Added commands for clearing and resetting constructor board

---
+++ ChessWPF/Commands/ClearBoardCommand.cs
@@ -0,0 +1,19 @@
+﻿using ChessWPF.ViewModels;
+
+namespace ChessWPF.Commands
+{
+    public class ClearBoardCommand : CommandBase
+    {
+        private readonly BoardConstructorMenuViewModel boardConstructorMenuViewModel;
+
+        public ClearBoardCommand(BoardConstructorMenuViewModel boardConstructorMenuViewModel)
+        {
+            this.boardConstructorMenuViewModel = boardConstructorMenuViewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            boardConstructorMenuViewModel.ResetBoard();
+        }
+    }
+}
---
+++ ChessWPF/Commands/ResetBoardToDefaultCommand.cs
@@ -0,0 +1,19 @@
+﻿using ChessWPF.ViewModels;
+
+namespace ChessWPF.Commands
+{
+    public class ResetBoardToDefaultCommand : CommandBase
+    {
+        private readonly BoardConstructorMenuViewModel boardConstructorMenuViewModel;
+
+        public ResetBoardToDefaultCommand(BoardConstructorMenuViewModel boardConstructorMenuViewModel)
+        {
+            this.boardConstructorMenuViewModel = boardConstructorMenuViewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            boardConstructorMenuViewModel.ResetBoard();
+        }
+    }
+}
