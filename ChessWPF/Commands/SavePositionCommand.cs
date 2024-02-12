64e6ccf942dd486beab7285186bc9ee838a1cffd
SvetlozarStoyanov <svetlio214@gmail.com>
2024-02-12 18:23:37 +0200

    New command for saving position in BoardConstructor

---
+++ ChessWPF/Commands/SavePositionCommand.cs
@@ -0,0 +1,19 @@
+﻿using ChessWPF.ViewModels;
+
+namespace ChessWPF.Commands
+{
+    public class SavePositionCommand : CommandBase
+    {
+        private readonly BoardConstructorMenuViewModel boardConstructorMenuViewModel;
+
+        public SavePositionCommand(BoardConstructorMenuViewModel boardConstructorMenuViewModel)
+        {
+            this.boardConstructorMenuViewModel = boardConstructorMenuViewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            boardConstructorMenuViewModel.SavePosition();
+        }
+    }
+}
