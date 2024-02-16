775cf8330487fdded851a248b7bf2058a21b53a3
SvetlozarStoyanov <svetlio214@gmail.com>
2024-02-16 15:05:43 +0200

    New event args class for position status

---
+++ ChessWPF/HelperClasses/CustomEventArgs/PositionStatusEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ChessWPF.HelperClasses.CustomEventArgs
+{
+    public class PositionStatusEventArgs : EventArgs
+    {
+        public PositionStatusEventArgs(string? errorMessage)
+        {
+            ErrorMessage = errorMessage;
+        }
+
+        public string? ErrorMessage { get; init; }
+
+    }
+}
