2eae46e3db31c7c9137ca5c1e0e77a2eabe407f4
SvetlozarStoyanov <svetlio214@gmail.com>
2024-10-11 18:37:28 +0300

    Added Views for MoveBoxes and MoveGrids

---
+++ ChessWPF/Views/Moves/MoveBox.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ChessWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for MoveBox.xaml
+    /// </summary>
+    public partial class MoveBox : UserControl
+    {
+        public MoveBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
