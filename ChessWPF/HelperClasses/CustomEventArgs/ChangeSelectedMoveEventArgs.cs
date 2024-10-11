651af72c6a6c848835bc0f098abe52f84b95df1f
SvetlozarStoyanov <svetlio214@gmail.com>
2024-10-11 18:36:52 +0300

    Added event args for Changing selected Moves

---
+++ ChessWPF/HelperClasses/CustomEventArgs/ChangeSelectedMoveEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using ChessWPF.Models.Moves;
+using System;
+
+namespace ChessWPF.HelperClasses.CustomEventArgs
+{
+    public class ChangeSelectedMoveEventArgs : EventArgs
+    {
+        public ChangeSelectedMoveEventArgs(Move move)
+        {
+            Move = move;
+        }
+
+        public Move Move { get; set; }
+    }
+}
