a161920a5808d41ad1bb947dabbc116988c49eba
SvetlozarStoyanov <svetlio214@gmail.com>
2024-10-11 18:37:50 +0300

    Added ViewModels for MoveBoxes and MoveGrids

---
+++ ChessWPF/ViewModels/Moves/MoveBoxViewModel.cs
@@ -0,0 +1,42 @@
+﻿using ChessWPF.Models.Moves;
+using System;
+using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace ChessWPF.ViewModels.Moves
+{
+    public class MoveBoxViewModel : ViewModelBase
+    {
+        private Move move;
+
+        public MoveBoxViewModel(Move move, SolidColorBrush defaultColor, SolidColorBrush selectedColor)
+        {
+            this.DefaultColor = Brushes.Gray;
+            this.SelectedColor = Brushes.LightGreen;
+            this.move = move;
+        }
+
+        public Move Move
+        {
+            get { return move; }
+            init { move = value; }
+        }
+
+        public MoveBoxViewModel Parent { get; set; }
+        public List<MoveBoxViewModel> Children { get; set; }
+        public SolidColorBrush BackgroundColor { get; set; }
+        public SolidColorBrush DefaultColor { get; set; }
+        public SolidColorBrush SelectedColor { get; set; }
+        public bool IsSelected { get; set; }
+
+        public event SelectMoveEventHandler SelectMove;
+        public delegate void SelectMoveEventHandler(object sender, EventArgs e);
+
+        public string Annotation => move.Annotation;
+
+        public void Select()
+        {
+            SelectMove?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ ChessWPF/ViewModels/Moves/MoveGridViewModel.cs
@@ -0,0 +1,50 @@
+﻿using ChessWPF.HelperClasses.CustomEventArgs;
+using System;
+
+namespace ChessWPF.ViewModels.Moves
+{
+    public class MoveGridViewModel : ViewModelBase
+    {
+        private MoveBoxViewModel movesTree;
+
+        public MoveGridViewModel()
+        {
+
+        }
+
+        public MoveBoxViewModel MovesTree
+        {
+            get { return movesTree; }
+            set { movesTree = value; }
+        }
+
+        public event ChangeSelectedMoveEventHandler ChangeSelectedMove;
+        public delegate void ChangeSelectedMoveEventHandler (object? sender, ChangeSelectedMoveEventArgs e);
+
+        public void AddMove(MoveBoxViewModel moveBoxViewModel)
+        {
+            MovesTree?.Children.Add(moveBoxViewModel);
+            moveBoxViewModel.Parent = MovesTree!;
+            moveBoxViewModel.SelectMove += SelectMove;
+            MovesTree = moveBoxViewModel;
+        }
+
+        public void SelectMove(object sender, EventArgs e)
+        {
+            if (sender == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            var movesBoxViewModel = sender as MoveBoxViewModel;
+
+            MovesTree.IsSelected = false;
+
+            movesBoxViewModel.IsSelected = true;
+
+            MovesTree = movesBoxViewModel;
+
+            ChangeSelectedMove?.Invoke(null, new ChangeSelectedMoveEventArgs(movesBoxViewModel.Move));
+        }
+    }
+}
